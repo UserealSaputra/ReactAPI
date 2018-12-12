4c53264ffb49bede324712f866cab193e48c3385
UserealSaputra <yusrilxanaxal244@gmail.com>
2018-12-12 14:43:07 +0700

    Ganti

--- src/component/PersonList.js
+++ src/component/PersonList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+// import cors from 'cors';
 // import Header from './Header';
 // import Axi from './Axi';
 
@@ -9,11 +10,11 @@ export default class PersonList extends Component {
     }
 
     componentDidMount() {
-        const req = "https://jsonplaceholder.typicode.com/users";
+        const req = "https://seneu.kejar.id/un/fe/sudahlatihanto/1/888888";
         // var xtep = new XMLHttpRequest();
         // xtep.open('GET', req, true);
         // xtep.send();
-        var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3NlbmV1LmtlamFyLmlkL3VuL3Npc3dhL2xvZ2luIiwiaWF0IjoxNTQ0NTg0NDE0LCJuYmYiOjE1NDQ1ODQ0MTQsImp0aSI6InJoUDVHZ1VKT0FXZGtJR0UiLCJzdWIiOiI4ODg4ODgiLCJwcnYiOiIyYjBkZTQ3M2YwOGE4NTU3MjBlNGI0YjMxMDhmMTFmNWI2NjVjMGUwIn0.nmmCfLW1rKYgsqjctDf-FFFoVSXVYvn-Awyu4Ozqmlg";
+        var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3NlbmV1LmtlamFyLmlkL3VuL3Npc3dhL2xvZ2luIiwiaWF0IjoxNTQ0NTk0NDk1LCJuYmYiOjE1NDQ1OTQ0OTUsImp0aSI6InpNenBQcDZrdXJKcWtNenYiLCJzdWIiOiI4ODg4ODgiLCJwcnYiOiIyYjBkZTQ3M2YwOGE4NTU3MjBlNGI0YjMxMDhmMTFmNWI2NjVjMGUwIn0.u3QeTwgxXM8LrpZGlcf3mCrki6DXEg2vj7rkB4IU_Lg";
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
         axios.defaults.timeout = 30000;
         axios.defaults.headers.common['Content-Type'] = 'application/json';
@@ -25,7 +26,7 @@ export default class PersonList extends Component {
         axios.get(req)
             .then(res => {
                 var nom = JSON.stringify(res);
-                console.log(res);
+                console.log(nom);
             })
             .catch((error) => {
                 console.log("An error has occoured " + error);
